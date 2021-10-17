5faaad15ba3d32b73256ef243bc2e9b4181278ef
Vasu Peddinti <v9493394558s@gmail.com>
2021-10-17 15:01:30 +0530

    dashboard modified

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { SIZE_TO_MEDIA } from '@ionic/core/dist/collection/utils/media'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { SIZE_TO_MEDIA } from '@ionic/core/dist/collection/utils/media';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,7 +9,11 @@ import { SIZE_TO_MEDIA } from '@ionic/core/dist/collection/utils/media'
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('slideWithNav', { static: false }) slideWithNav: IonSlides;
+  @ViewChild('slideWithNav2', { static: false }) slideWithNav2: IonSlides;
+  @ViewChild('slideWithNav3', { static: false }) slideWithNav3: IonSlides;
+
+ 
 
   ngOnInit() {
   }
@@ -20,4 +25,137 @@ export class DashboardPage implements OnInit {
         splitPane.classList.toggle('split-pane-visible');
   }
 
+
+
+  sliderOne: any;
+  sliderTwo: any;
+  sliderThree: any;
+
+
+  //Configuration for each Slider
+  slideOptsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true
+  };
+  slideOptsTwo = {
+    initialSlide: 1,
+    slidesPerView: 2,
+    loop: true,
+    centeredSlides: true,
+    spaceBetween: 20
+  };
+  slideOptsThree = {
+    initialSlide: 0,
+    slidesPerView: 3
+  };
+
+  constructor(
+  ) {
+    //Item object for Nature
+    this.sliderOne =
+    {
+      isBeginningSlide: true,
+      isEndSlide: false,
+      slidesItems: [
+        {
+          id: 995
+        },
+        {
+          id: 925
+        },
+        {
+          id: 940
+        },
+        {
+          id: 943
+        },
+        {
+          id: 944
+        }
+      ]
+    };
+    //Item object for Food
+    this.sliderTwo =
+    {
+      isBeginningSlide: true,
+      isEndSlide: false,
+      slidesItems: [
+        {
+          id: 324
+        },
+        {
+          id: 321
+        },
+        {
+          id: 435
+        },
+        {
+          id: 524
+        },
+        {
+          id: 235
+        }
+      ]
+    };
+    //Item object for Fashion
+    this.sliderThree =
+    {
+      isBeginningSlide: true,
+      isEndSlide: false,
+      slidesItems: [
+        {
+          id: 643
+        },
+        {
+          id: 532
+        },
+        {
+          id: 232
+        },
+        {
+          id: 874
+        },
+        {
+          id: 193
+        }
+      ]
+    };
+  }
+
+  //Move to Next slide
+  slideNext(object, slideView) {
+    slideView.slideNext(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });
+  }
+
+  //Move to previous slide
+  slidePrev(object, slideView) {
+    slideView.slidePrev(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });;
+  }
+
+  //Method called when slide is changed by drag or navigation
+  SlideDidChange(object, slideView) {
+    this.checkIfNavDisabled(object, slideView);
+  }
+
+  //Call methods to check if slide is first or last to enable disbale navigation  
+  checkIfNavDisabled(object, slideView) {
+    this.checkisBeginning(object, slideView);
+    this.checkisEnd(object, slideView);
+  }
+
+  checkisBeginning(object, slideView) {
+    slideView.isBeginning().then((istrue) => {
+      object.isBeginningSlide = istrue;
+    });
+  }
+  checkisEnd(object, slideView) {
+    slideView.isEnd().then((istrue) => {
+      object.isEndSlide = istrue;
+    });
+  }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SIZE_TO_MEDIA } from '@ionic/core/dist/collection/utils/media';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,13 @@ export class LoginPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleMenu(){
+    const splitPane = document.querySelector('ion-split-pane');
+    
+    if (window.matchMedia(SIZE_TO_MEDIA[splitPane.when] || splitPane.when).matches)
+        splitPane.classList.toggle('split-pane-visible');
   }
 
 }
